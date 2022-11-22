000edc4ce8c4da6ca9902d2baa84e21f5dfc8244
joseperu2503 <51133263+joseperu2503@users.noreply.github.com>
2022-11-21 19:40:56 -0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Account from './Pages/Account'
 import Home from './Pages/Home'
+import Login from './Pages/Login'
+import Signup from './Pages/Signup'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +14,9 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/account' element={<Account/>}/>
       </Routes>
     </>
   )
